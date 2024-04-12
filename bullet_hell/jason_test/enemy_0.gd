d60a26576210b63b4bdcc8ffe05ed3abc7408b7e
draynr <dangjason9812@gmail.com>
2024-04-11 21:42:15 -0700

    LOS

--- bullet_hell/jason_test/enemy_0.gd
+++ bullet_hell/jason_test/enemy_0.gd
@@ -9,38 +9,35 @@ var ray_count = 5
 var view_distance = 20.0
 var view_angle = 45.0
 
+var target
+const turn_speed = 2
+
 @onready var line3d = $Line3D
 @onready var los = $LineOfSight
 @onready var raycast: RayCast3D = $RayCast3D
+@onready var vision = $vision
+
+enum {IDLE, ALERT}
 
 var see_player = false
+var state = IDLE
 
 func _ready():
-	player = get_parent().get_node("player").position
+	pass
 
 func _physics_process(delta):
-	# var dir = Vector3()
 
-	see_player = check_los()
-
-	# nav.target_position = get_parent().get_node("player").position
-	# dir = nav.get_next_path_position() - global_position
-	# dir = dir.normalized()
-
-	# velocity = velocity.lerp(dir * speed, accel * delta)
+	match state:
+		ALERT:
+			vision.look_at(target.global_transform.origin, Vector3.UP)
+			rotate_y(deg_to_rad(vision.rotation.y * turn_speed))
 
 	move_and_slide()
+func _on_area_3d_body_entered(body):
+	# pass # Replace with function body.
+	if body.is_in_group("player"):
+		state = ALERT
+		target = body
 
-func check_los() -> bool:
-	var player_pos = player.global_transform.origin
-	var dir = (player_pos - global_transform.origin).normalized()
-
-	var can_see = false
-	
-	for i in range(ray_count):
-		var angle = lerp( - view_angle / 2, view_angle / 2, float(i) / (ray_count - 1))
-		var rotated_direction: Vector3 = direction_to_player.rotated(Vector3.UP, deg_to_rad(angle))
-		raycast.cast_to = rotated_direction * view_distance
-		raycast.force_raycast_update()
-
-	return can_see
+func _on_area_3d_body_exited(body):
+	state = IDLE
