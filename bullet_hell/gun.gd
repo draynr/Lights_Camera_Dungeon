05b10c39ee6f1baf60d373580a4a9a6099a479f5
Juno <junolee64@gmail.com>
2024-04-14 00:30:10 -0700

    fix gun rotattion

--- bullet_hell/gun.gd
+++ bullet_hell/gun.gd
@@ -7,3 +7,9 @@ func aim_gun(player, target_pos, gun):
 	gun.global_transform.origin = player.global_transform.origin  # Ensure the gun starts at the player's position
 	var look_target = player.global_transform.origin + adjusted_pos
 	gun.look_at(player.global_transform.origin + adjusted_pos, up)
+	if abs(gun.global_rotation_degrees[1]) < 90.:
+		gun.global_rotation_degrees[0] = -45.
+	else:
+		gun.global_rotation_degrees[0] = 135.
+		#gun.global_rotation_degrees[2] *= -1
+	print(gun.global_rotation_degrees)
